5e9c5b5d9060ba02b0ce3393aaff8b8f8c31ae11
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-05-23 15:50:35 +0800

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState ,useRef} from 'react'
 import vismotor from '../public/vismotor.png'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
@@ -8,9 +8,13 @@ const login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false); 
+    const [showToast, setShowToast] = useState(false);  
+    const toastRef = useRef(null);
     const navigate = useNavigate();
     const handleLogin = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
     console.log(BASE_URL);
         try {
           const response = await axios.post(`${BASE_URL}/login`, {
@@ -29,11 +33,28 @@ const login = () => {
         } catch (err) {
           // Handle login error
           setError('Invalid credentials');
+          setIsLoading(false); 
+          setShowToast(true);
+
+          // Hide the toast notification after 3 seconds
+          setTimeout(() => {
+            setShowToast(false);
+          }, 3000);
           console.error(err);
-        }
+        } finally {
+            setIsLoading(false); // Set isLoading to false after the login process is completed
+          }
       };
     return (
         <div className='text-black bg-white w-screen h-screen'>
+               <div
+        ref={toastRef}
+        className={`fixed top-5 left-4 z-50 rounded-lg bg-red-500 px-4 py-3 text-white transition-all duration-300 ${
+          showToast ? 'translate-x-10' : 'translate-x-[-120%]'
+        }`}
+      >
+        <p>Invalid credentials. Please try again.</p>
+      </div>
             <div className='p-4'>
             <div className="relative flex flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
           <div className='relative mt-14'>
@@ -97,8 +118,11 @@ const login = () => {
         </p>
       </label>
     </div>
-    <button type='submit' className="bg-red-400 mt-6 w-full select-none p-2 rounded-lg font-bold  py-3 px-6 text-center align-middle font-sans text-xs uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-         Login
+    <button type='submit' className="flex justify-center items-center bg-red-400 mt-6 w-full select-none p-2 rounded-lg font-bold  py-3 px-6 text-center align-middle font-sans text-xs uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+    {isLoading && <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>} Login
         </button>
   
   
