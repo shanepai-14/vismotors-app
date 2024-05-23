7427cd65560ded6de99020cd0534df64fa5bc597
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-05-23 14:43:52 +0800

    Update utils.jsx

--- src/utils.jsx
+++ src/utils.jsx
@@ -7,8 +7,8 @@ export const isAuthenticated = () => {
 };
 
 export const processEnv = process.env;
-export const BASE_URL = processEnv.REACT_APP_BASE_URL;
-export const IMAGE_URL = processEnv.REACT_APP_IMAGE_URL;
+export const BASE_URL = 'https://5c82-143-44-184-72.ngrok-free.app/api';
+export const IMAGE_URL = 'https://5c82-143-44-184-72.ngrok-free.app';
 
 export const userData = JSON.parse(localStorage.getItem('userData'));
 export const userProfile = JSON.parse(localStorage.getItem('userProfile'));
