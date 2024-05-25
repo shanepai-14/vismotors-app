5f08e1222d6bb3fc63c49e6cc43dce887490dadd
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-05-25 15:11:51 +0800

    Update link.jsx

--- src/pages/link/link.jsx
+++ src/pages/link/link.jsx
@@ -30,7 +30,7 @@ const link = () => {
                 <div className="relative flex h-32 w-full justify-center rounded-xl bg-cover" >
                     <img src='https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/media/banner.ef572d78f29b0fee0a09.png' className="absolute flex h-32 w-full justify-center rounded-xl bg-cover"/> 
                     <div className="absolute -bottom-12 flex h-[87px] w-[87px] items-center justify-center rounded-full border-[4px] border-white bg-pink-400 dark:!border-navy-700">
-                        <img className="h-full w-full rounded-full" src={`${IMAGE_URL}/storage/temporary_docs/${profileData.profile_picture}`} alt="" />
+                        <img className="h-full w-full rounded-full" src={`${IMAGE_URL}/storage/public/temporary_docs/${profileData.profile_picture}`} alt="" />
                     </div>
                 </div> 
                 <div className="mt-16 flex flex-col items-center">
